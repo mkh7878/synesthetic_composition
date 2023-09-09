a0ed60b37ccae51f1059ed1ace31ac14d690b930
Mae Horak <70494117+mkh7878@users.noreply.github.com>
2023-09-09 16:19:24 +0100

    Add files via upload

--- midi_to_circle/video_output/images.py
+++ midi_to_circle/video_output/images.py
@@ -1,24 +1,30 @@
 from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.core.window import Window
-from kivy.clock import Clock  # Import Clock for scheduled updates
-from sound_inputs import intervals
+from kivy.clock import Clock
+from sound_inputs import intervals  # You may need to adjust this import
+from collections import deque
 
 class ImageSwitcherApp(App):
     def build(self):
-        self.frames = ["frames/image1.png", "frames/image2.png", "frames/image3.png", "frames/image4.png"]
+        self.frames = [
+            f'output_images/img{x}_{y}.png' for x in range(20) for y in range(20)
+        ]
         self.current_frame = 0
+        self.frame_rate = 30  # Frames per second
+        self.frame_interval = 1.0 / self.frame_rate
 
         # Create a RelativeLayout to hold the Image widget
         self.layout = RelativeLayout()
 
         # Create an Image widget and add it to the layout
-        self.image = Image(source=self.frames[self.current_frame], allow_stretch=True, keep_ratio=False)
+        self.image = Image(
+            source=self.frames[self.current_frame], allow_stretch=True, keep_ratio=False
+        )
         self.layout.add_widget(self.image)
 
         # Schedule the image update method
-        Clock.schedule_interval(self.update_image, 1.0 / 30.0)  # Update every 1/30th of a second
+        Clock.schedule_interval(self.update_image, self.frame_interval)
 
         return self.layout
 
@@ -29,21 +35,25 @@ class ImageSwitcherApp(App):
         if len(intervals.captured_notes) > 1:
             current_interval = intervals.captured_notes[-1]  # Most recently captured note
 
-            # Based on "mood" of intervals.
+            # Determine the next frame based on intervals
+            next_frame = self.current_frame  # Initialize with the current frame
+
             if current_interval in [0, 4, 5]:
-                self.current_frame = 0
-
+                if next_frame % 20 != 19:
+                    next_frame += 1
             elif current_interval in [1, 6, 11]:
-                self.current_frame = 1
-
+                if next_frame % 20 != 0:
+                    next_frame -= 1
             elif current_interval in [2, 8, 9, 10]:
-                self.current_frame = 2
-
+                if next_frame // 20 != 19:
+                    next_frame += 20
             elif current_interval in [3, 7]:
-                self.current_frame = 3
+                if next_frame // 20 != 0:
+                    next_frame -= 20
 
-            # Update the displayed image
-            self.image.source = self.frames[self.current_frame]
+            if next_frame != self.current_frame:
+                self.current_frame = next_frame
+                self.image.source = self.frames[self.current_frame]
 
 if __name__ == '__main__':
     ImageSwitcherApp().run()
